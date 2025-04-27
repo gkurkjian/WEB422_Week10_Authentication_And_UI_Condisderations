8d88ccf800a9a6fa18ae29c063da427f9512ec1b
George K <gkurkjian@myseneca.ca>
2025-04-26 22:32:46 -0400

    eight commit: made login and logout in Navigation bar, when user enters will designate its token, when logs out, will remove the token and connect to home page

--- vehicles-UI/my-app/components/Navigation.js
+++ vehicles-UI/my-app/components/Navigation.js
@@ -22,18 +22,36 @@
 // This version it'll not cause of from hydration
 import { Container, Navbar, Nav } from "react-bootstrap";
 import Link from 'next/link';
+import { useRouter } from "next/router";
+import { readToken, removeToken } from "@/lib/authenticate";
 
-export default function Navigation() {
+export default function Navigation(props) {
+
+  const router = useRouter();
+  let token = readToken();
+
+  function logout() {
+    removeToken();
+    router.push("/");
+  }
+
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand as={Link} href="/">Vehicles UI</Navbar.Brand>
+        <Navbar.Brand as={Link} href="/">
+          Vehicles UI {token && <>- Welcome {token.userName}</>}
+        </Navbar.Brand>
 
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link as={Link} href="/">Home</Nav.Link>
-            <Nav.Link as={Link} href="/vehicles">Vehicles</Nav.Link>
+            {token && <Nav.Link as={Link} href="/vehicles">Vehicles</Nav.Link>}
+          </Nav>
+
+          <Nav className="ml-auto">
+            {!token && <Nav.Link as={Link} href="/login">Login</Nav.Link>}
+            {token && <Nav.Link onClick={logout}>Logout</Nav.Link>}
           </Nav>
         </Navbar.Collapse>
       </Container>
